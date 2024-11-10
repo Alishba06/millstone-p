9b429838b1a6addc41e0f93262437108340747df
Alishba06 <alishbainteligent46@gmail.com>
2024-11-11 02:51:11 +0500

    Removed import for Social component

--- src/app/herosection/page.tsx
+++ src/app/herosection/page.tsx
@@ -3,7 +3,7 @@
 import React from 'react'
 import Image from 'next/image'
 import {TypeAnimation} from 'react-type-animation'
-import Social from '../social/page'      
+  
 const HeroSection = () => {
     return (
       <div>
